4b2469b2cc6bb18fce6fa0ebb8a63fda5f900def
Kate F <kate@elide.org>
2014-11-21 22:08:21 +0000

    Mouse button lookup and dispatch.

---
+++ src/button.c
@@ -0,0 +1,93 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include "button.h"
+#include "cmd.h"
+
+struct button {
+	int mod;
+	char **argv;
+	struct button *next;
+};
+
+struct button *buttons[BUTTON_MAX];
+
+static struct button *
+button_find(struct button *head, int button, int mod)
+{
+	struct button *p;
+
+	for (p = head; p != NULL; p = p->next) {
+		if (p->mod == mod) {
+			return p;
+		}
+	}
+
+	return NULL;
+}
+
+static int
+button_add(struct button **head, int button, int mod, char *argv[])
+{
+	struct button *new;
+
+	assert(head != NULL);
+	assert(argv != NULL);
+
+	if (button < 0 || button > sizeof buttons / sizeof *buttons) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	if (button_find(*head, button, mod)) {
+		errno = EEXIST;
+		return -1;
+	}
+
+	new = malloc(sizeof *new);
+	if (new == NULL) {
+		return -1;
+	}
+
+/* TODO: duplicate argv */
+
+	new->mod    = mod;
+	new->argv   = argv;
+
+	new->next = *head;
+	*head = new;
+
+	return 0;
+}
+
+int
+button_bind(int button, int mod, char *argv[])
+{
+	if (button < 0 || button > sizeof buttons / sizeof *buttons) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	return button_add(&buttons[button], button, mod, argv);
+}
+
+int
+button_dispatch(int button, int mod)
+{
+	struct button *p;
+
+	if (button < 0 || button > sizeof buttons / sizeof *buttons) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	p = button_find(buttons[button], button, mod);
+	if (p == NULL) {
+		errno = ENOENT;
+		return -1;
+	}
+
+	return cmd_dispatch(p->argv);
+}
+
---
+++ src/button.h
@@ -0,0 +1,15 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#ifndef BUTTON_MAX
+#define BUTTON_MAX 16 /* XXX: from X? */
+#endif
+
+int
+button_bind(int button, int mod, char *argv[]);
+
+int
+button_dispatch(int button, int mod);
+
+#endif
+
