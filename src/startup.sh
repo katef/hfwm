2ec55878e2b93f8b202a639d3ad66b2c550d4a2f
Kate F <kate@elide.org>
2014-11-21 09:26:50 +0000

    Random background.

--- src/startup.sh
+++ src/startup.sh
@@ -3,10 +3,15 @@
 IPC_PATH=${IPC_PATH:-/tmp/hfwm.sock}
 
 hc() {
-	echo $* | socat -t0 unix-sendto:$IPC_PATH stdin
+	echo -n $* | socat -t0 unix-sendto:$IPC_PATH stdin
 }
 
-hc hello
-hc spawn xterm
+rand() {
+	dd if=/dev/urandom bs=1 count=1 2>/dev/null \
+	| od -t u4 \
+	| awk 'NR==1 { print $2 }' \
+	| ( read n; echo $(( 4 + $n % 2 )) )
+}
 
+hc spawn xsetroot -solid '#'`rand``rand``rand`
 
